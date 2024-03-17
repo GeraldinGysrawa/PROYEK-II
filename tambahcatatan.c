f641ec4c7c4199f027ab667066320587b0608f45
Geraldin Gysrawa <geraldingysrawa18@gmail.com>
2024-03-17 09:42:14 +0700

    memperbaharui file tambahcatatan dan merename file.h

---
+++ tambahcatatan.c
@@ -0,0 +1,40 @@
+#include "geraldin.h"
+#include <stdio.h>
+
+int nomorCatatanTerakhir = 0;
+
+void tambahCatatan() {
+
+    FILE *file = fopen("catatan.txt", "a");
+    if (file == NULL) {
+        printf("Error: Gagal membuka file.\n");
+        return;
+    }
+
+    struct Catatan catatan;
+    nomorCatatanTerakhir++;        
+
+    catatan.no = nomorCatatanTerakhir;
+
+    printf("Masukkan judul catatan: ");
+    scanf(" %[^\n]s", catatan.judul);
+
+    printf("Masukkan tanggal catatan (DD/MM/YYYY): ");
+    scanf(" %[^\n]s", catatan.tanggal);
+
+    printf("Masukkan isi catatan:\n");
+    scanf(" %[^\n]s", catatan.isi);
+
+    char kunci[MAX_KEY];
+    printf("Masukkan kunci enkripsi: ");
+    scanf(" %[^\n]s", kunci);
+
+    enkripsi(catatan.judul, kunci);
+    enkripsi(catatan.isi, kunci);
+    
+    fprintf(file, "%d|%s|%s|%s\n", catatan.no, catatan.judul, catatan.tanggal, catatan.isi);
+
+    fclose(file);
+
+    printf("Catatan berhasil ditambahkan.\n");
+}
