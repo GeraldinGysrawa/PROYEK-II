ec1a62d0e55eafdfc18579ace8f19ff0d5648854
nisa0520 <163684576+nisa0520@users.noreply.github.com>
2024-03-25 19:47:35 +0700

    Update sandi.c

--- sandi.c
+++ sandi.c
@@ -1,81 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "catatan.h"
+#include "sandi.h"
 
-void encryptPassword(char *password, int shift) {
+#define MAX_SANDI 100
+
+int shift = 3;
+
+void enkripsiSandi(char *sandi, int shift) {
     int i;
-    for (i = 0; password[i] != '\0'; ++i) {
-        if (password[i] >= 'A' && password[i] <= 'Z') {
-            password[i] = 'A' + ((password[i] - 'A' + shift) % 26);
+    for (i = 0; sandi[i] != '\0'; ++i) {
+        if (sandi[i] >= 'A' && sandi[i] <= 'Z') {
+            sandi[i] = 'A' + ((sandi[i] - 'A' + shift) % 26);
         }
-        else if (password[i] >= 'a' && password[i] <= 'z') {
-            password[i] = 'a' + ((password[i] - 'a' + shift) % 26);
+        else if (sandi[i] >= 'a' && sandi[i] <= 'z') {
+            sandi[i] = 'a' + ((sandi[i] - 'a' + shift) % 26);
         }
     }
 }
 
-void decryptPassword(char *password, int shift) {
+void dekripsiSandi(char *sandi, int shift) {
     int i;
-    for (i = 0; password[i] != '\0'; ++i) {
-        if (password[i] >= 'A' && password[i] <= 'Z') {
-            password[i] = 'A' + ((password[i] - 'A' + 26 - shift) % 26);
+    for (i = 0; sandi[i] != '\0'; ++i) {
+        if (sandi[i] >= 'A' && sandi[i] <= 'Z') {
+            sandi[i] = 'A' + ((sandi[i] - 'A' + 26 - shift) % 26);
         }
-        else if (password[i] >= 'a' && password[i] <= 'z') {
-            password[i] = 'a' + ((password[i] - 'a' + 26 - shift) % 26);
+        else if (sandi[i] >= 'a' && sandi[i] <= 'z') {
+            sandi[i] = 'a' + ((sandi[i] - 'a' + 26 - shift) % 26);
         }
     }
 }
 
-void readEncryptedPassword(char *encryptedPassword) {
-    FILE *file = fopen("sandi.txt", "r");
+void saveEncryptedPassword(char *sandiTerenkripsi) {
+    FILE *file = fopen("catatan.txt", "a+");
     if (file != NULL) {
-        fgets(encryptedPassword, 100, file);
-        fclose(file);
-    }
-}
-
-void saveEncryptedPassword(char *encryptedPassword) {
-    FILE *file = fopen("sandi.txt", "a+");
-    if (file != NULL) {
-        fprintf(file, "%s\n", encryptedPassword);
+        fprintf(file, "%s\n", sandiTerenkripsi);
         fclose(file);
     } else {
         printf("Gagal membuat atau membuka file untuk penyimpanan sandi.\n");
     }
 }
 
-int main() {
-    char password[100];
-    int shift = 3;
+void untukSandi() {
+    char sandi[100];
     printf("Masukkan sandi: ");
-    fgets(password, sizeof(password), stdin);
-    password[strcspn(password, "\n")] = '\0';
+    fgets(sandi, sizeof(sandi), stdin);
+    sandi[strcspn(sandi, "\n")] = '\0';
 
-    encryptPassword(password, shift);
+    enkripsiSandi(sandi, shift);
 
-    saveEncryptedPassword(password);
+    saveEncryptedPassword(sandi);
     printf("Catatan telah dikunci.\n");
 
     printf("Masukkan sandi untuk membuka catatan: ");
-    fgets(password, sizeof(password), stdin);
-    password[strcspn(password, "\n")] = '\0'; 
+    fgets(sandi, sizeof(sandi), stdin);
+    sandi[strcspn(sandi, "\n")] = '\0'; 
 
-    FILE *file = fopen("sandi.txt", "r");
+    FILE *file = fopen("catatan.txt", "r");
     char storedEncryptedPassword[100];
     if (file != NULL) {
         while (fscanf(file, "%s", storedEncryptedPassword) != EOF) {
-            decryptPassword(storedEncryptedPassword, shift);
-            if (strcmp(password, storedEncryptedPassword) == 0) {
+            dekripsiSandi(storedEncryptedPassword, shift);
+            if (strcmp(sandi, storedEncryptedPassword) == 0) {
                 printf("Catatan telah terbuka.\n");
                 fclose(file);
-                return 0;
             }
         }
         fclose(file);
     }
 
     printf("Kata sandi salah. Catatan gagal dibuka.\n");
-
-    return 0;
 }
