bd0cdd20f5764aa52fd5375c06bb6931646b2f46
nisa0520 <163684576+nisa0520@users.noreply.github.com>
2024-03-26 12:30:59 +0700

    Update annisa.h
    Update baru

--- annisa.h
+++ annisa.h
@@ -1,9 +1,13 @@
 #ifndef ANNISA_H
 #define ANNISA_H
+#include "catatan.h"
+
+#define MAX_SANDI 100
 
 void enkripsiSandi(char *sandi, int shift);
 void dekripsiSandi(char *sandi, int shift);
+void untukSandi();
+void tampilSandi();
 void saveEncryptedPassword(char *sandiTerenkripsi);
 
-#endif 
-
+#endif /* SANDI_H */
